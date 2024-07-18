b9a729df80f6594adef7f0091fc1a0c5b16ad9a4
gibberish <gbjgms@gmail.com>
2024-07-18 21:24:33 +0700

    Modmain code cleanup + New screenshots

--- modmenu/modmain.lua
+++ modmenu/modmain.lua
@@ -21,104 +21,96 @@ local SELECTED_SORTING_METHOD = DEFAULT_SORTING_METHOD
 
 local function OptionsScreen_AddModsTab(self)
     if self.nav_tabs ~= nil and self.tabs ~= nil and self.scrollContents ~= nil then
-        local function _LayoutModEntries()
-            self.pages.mods.mod_entries:RemoveAllChildren()
 
-            -- add mod entries
-            local function _AddModEntry(modname)
-                local mod_fancyname = GLOBAL.KnownModIndex:GetModFancyName(modname)
-                local mod_description = "No description"
-                local mod_author = "unknown"
-                local mod_version = "unknown"
-                local mod_enabled = false
-                if GLOBAL.KnownModIndex:GetModInfo(modname) and GLOBAL.KnownModIndex:GetModInfo(modname).description then
-                    mod_description = GLOBAL.KnownModIndex:GetModInfo(modname).description
-                end
-                if GLOBAL.KnownModIndex:GetModInfo(modname) and GLOBAL.KnownModIndex:GetModInfo(modname).author then
-                    mod_author = GLOBAL.KnownModIndex:GetModInfo(modname).author
-                end
-                if GLOBAL.KnownModIndex:GetModInfo(modname) and GLOBAL.KnownModIndex:GetModInfo(modname).version then
-                    mod_version = GLOBAL.KnownModIndex:GetModInfo(modname).version
-                end
-                if GLOBAL.KnownModIndex and GLOBAL.KnownModIndex.savedata and GLOBAL.KnownModIndex.savedata.known_mods and GLOBAL.KnownModIndex.savedata.known_mods[modname] and GLOBAL.KnownModIndex.savedata.known_mods[modname].enabled then
-                    mod_enabled = true
-                end
-                local entry_desc = mod_description.."\n<i>Author: "..mod_author.."\nVersion: "..mod_version.."\nDirectory: "..modname.."</i>"
-                local mod_is_favorited = GLOBAL.Profile:IsModFavorited(modname)
-                local mod_has_configurations = GLOBAL.KnownModIndex:HasModConfigurationOptions(modname)
-                
-                local entry = self.pages.mods.mod_entries:AddChild(Widget("Mod Entry Row"))
-                entry.entry_main = entry:AddChild(OptionsScreenToggleRow(self.rowWidth - MOD_ENTRY_BUTTON_WIDTH - 10))
-                    :SetText(mod_fancyname)
-                    :SetValues({
-                        { desc = entry_desc, data = true },
-                        { desc = entry_desc, data = false }
-                    })
-                    :_SetValue(mod_enabled and 1 or 2)
-                    :SetOnValueChangeFn(function(data)
-                        if GLOBAL.KnownModIndex and GLOBAL.KnownModIndex.savedata and GLOBAL.KnownModIndex.savedata.known_mods and GLOBAL.KnownModIndex.savedata.known_mods[modname] and GLOBAL.KnownModIndex.savedata.known_mods[modname].enabled ~= nil then
-                            if data then 
-                                GLOBAL.KnownModIndex:Enable(modname)
-                            else
-                                GLOBAL.KnownModIndex:Disable(modname)
-                            end
-                        end
-                    end)
-
-                entry.sidebuttons_container = entry:AddChild(Widget("Mod Entry Side Buttons Container"))
-                -- offset the icons inside the mod row's side buttons
-                if mod_has_configurations then
-                    -- favorite button
-                    entry.sidebuttons_container:AddChild(ModEntryImageButtonToggle("images/icons_ftf/stat_health.tex", MOD_ENTRY_BUTTON_WIDTH, (entry.entry_main.height/2 - 10)))
-                        :SetImageOffset(4, 4)
-                        :SetValues({
-                            {data = true},
-                            {data = false}
-                        })
-                        :_SetValue(mod_is_favorited and 1 or 2)
-                        :OnFocusChange(false) -- to help the button updates its image color after setting its values manually
-                        :SetOnValueChangeFn(function(data)
-                            GLOBAL.Profile:SetModFavorited(modname, data)
-                            GLOBAL.Profile.dirty = true
-                            GLOBAL.Profile:Save()
-
-                            if SELECTED_SORTING_METHOD == 7 then
-                                _LayoutModEntries()
-                            end
-                        end)
-                    -- mod configs button
-                    entry.sidebuttons_container:AddChild(ModEntryImageButton("images/ui_ftf_dialog/ic_options.tex", MOD_ENTRY_BUTTON_WIDTH, (entry.entry_main.height/2 - 10)))
-                        :SetImageOffset(4, 4)
-                        :SetOnClickFn(function()
-                            local configurator_screen = ModConfiguratorScreen(modname)
-                            GLOBAL.TheFrontEnd:PushScreen(configurator_screen)
-                        end)
-
-                    entry.sidebuttons_container:LayoutChildrenInColumn(10)
-                else
-                    -- favorite button
-                    entry.sidebuttons_container:AddChild(ModEntryImageButtonToggle("images/icons_ftf/stat_health.tex", MOD_ENTRY_BUTTON_WIDTH, entry.entry_main.height))
-                        :SetImageOffset(4, 4)
-                        :SetValues({
-                            {data = true},
-                            {data = false}
-                        })
-                        :_SetValue(mod_is_favorited and 1 or 2)
-                        :OnFocusChange(false) -- to help the button updates its image color after setting its values manually
-                        :SetOnValueChangeFn(function(data)
-                            GLOBAL.Profile:SetModFavorited(modname, data)
-                            GLOBAL.Profile.dirty = true
-                            GLOBAL.Profile:Save()
-
-                            if SELECTED_SORTING_METHOD == 7 then
-                                _LayoutModEntries()
-                            end
-                        end)
-                end
-
-                entry:LayoutChildrenInRow(10)
+        local function AddModEntry(modname)
+            local mod_fancyname = GLOBAL.KnownModIndex:GetModFancyName(modname)
+            local mod_description = "No description"
+            local mod_author = "unknown"
+            local mod_version = "unknown"
+            local mod_enabled = false
+            if GLOBAL.KnownModIndex:GetModInfo(modname) and GLOBAL.KnownModIndex:GetModInfo(modname).description then
+                mod_description = GLOBAL.KnownModIndex:GetModInfo(modname).description
             end
+            if GLOBAL.KnownModIndex:GetModInfo(modname) and GLOBAL.KnownModIndex:GetModInfo(modname).author then
+                mod_author = GLOBAL.KnownModIndex:GetModInfo(modname).author
+            end
+            if GLOBAL.KnownModIndex:GetModInfo(modname) and GLOBAL.KnownModIndex:GetModInfo(modname).version then
+                mod_version = GLOBAL.KnownModIndex:GetModInfo(modname).version
+            end
+            if GLOBAL.KnownModIndex and GLOBAL.KnownModIndex.savedata and GLOBAL.KnownModIndex.savedata.known_mods and GLOBAL.KnownModIndex.savedata.known_mods[modname] and GLOBAL.KnownModIndex.savedata.known_mods[modname].enabled then
+                mod_enabled = true
+            end
+            local entry_desc = mod_description.."\n<i>Author: "..mod_author.."\nVersion: "..mod_version.."\nDirectory: "..modname.."</i>"
+            local mod_is_favorited = GLOBAL.Profile:IsModFavorited(modname)
+            local mod_has_configurations = GLOBAL.KnownModIndex:HasModConfigurationOptions(modname)
+            
+            local entry = self.pages.mods.mod_entries:AddChild(Widget("Mod Entry Row"))
+            entry.entry_main = entry:AddChild(OptionsScreenToggleRow(self.rowWidth - MOD_ENTRY_BUTTON_WIDTH - 10))
+                :SetText(mod_fancyname)
+                :SetValues({
+                    { desc = entry_desc, data = true },
+                    { desc = entry_desc, data = false }
+                })
+                :_SetValue(mod_enabled and 1 or 2)
+                :SetOnValueChangeFn(function(data)
+                    if GLOBAL.KnownModIndex and GLOBAL.KnownModIndex.savedata and GLOBAL.KnownModIndex.savedata.known_mods and GLOBAL.KnownModIndex.savedata.known_mods[modname] and GLOBAL.KnownModIndex.savedata.known_mods[modname].enabled ~= nil then
+                        if data then 
+                            GLOBAL.KnownModIndex:Enable(modname)
+                        else
+                            GLOBAL.KnownModIndex:Disable(modname)
+                        end
+                    end
+                end)
 
+            -- adding side buttons to a mod entry (the buttosn to the right)
+            entry.sidebuttons_container = entry:AddChild(Widget("Mod Entry Side Buttons Container"))
+            -- full height if we only need to add a favorite button, half height
+            -- if we have a configs button, because they would stack on top of each other
+            local sidebutton_height = mod_has_configurations and (entry.entry_main.height/2 - 10) or entry.entry_main.height
+            -- adding favorite button
+            entry.sidebuttons_container:AddChild(ModEntryImageButtonToggle("images/icons_ftf/stat_health.tex", MOD_ENTRY_BUTTON_WIDTH, sidebutton_height))
+                :SetImageOffset(4, 4)
+                :SetValues({
+                    {data = true},
+                    {data = false}
+                })
+                :_SetValue(mod_is_favorited and 1 or 2)
+                :OnFocusChange(false) -- to help the button updates its image color after setting its values manually
+                :SetOnValueChangeFn(function(data)
+                    GLOBAL.Profile:SetModFavorited(modname, data)
+                    GLOBAL.Profile.dirty = true
+                    GLOBAL.Profile:Save()
+
+                    -- re-sort the mod list if we're selecting sort by favorites
+                    if SELECTED_SORTING_METHOD == 7 then
+                        LayoutModEntries()
+                    end
+                end)
+
+            if mod_has_configurations then
+                -- adding mod configs button
+                entry.sidebuttons_container:AddChild(ModEntryImageButton("images/ui_ftf_dialog/ic_options.tex", MOD_ENTRY_BUTTON_WIDTH, sidebutton_height))
+                    :SetImageOffset(4, 4)
+                    :SetOnClickFn(function()
+                        local configurator_screen = ModConfiguratorScreen(modname)
+                        GLOBAL.TheFrontEnd:PushScreen(configurator_screen)
+                    end)
+            end
+            
+            entry.sidebuttons_container:LayoutChildrenInColumn(10)
+            entry:LayoutChildrenInRow(10)
+        end
+
+        local function LayoutModEntries()
+            self.pages.mods.mod_entries:RemoveAllChildren()            
+
+            --NOTE: there are basically no differences between client and server
+            -- mods, its just a thing left behind from the DST modding system, 
+            -- we dont have server/client hosting in rotwood so it doesn't matter
+            -- but you can still set that property in your mod and it will be
+            -- categorized based on that because why not.
+
+            -- CLIENT MODS
             self.pages.mods.mod_entries:AddChild(OptionsScreenCategoryTitle(self.rowWidth, "Client Mods"))
             -- mods are shown sorted by their fancy name (the name defined in modinfo), if there is no fancy name, the moddir name is used
             local list_client = GLOBAL.KnownModIndex:GetClientModNames()
@@ -132,8 +124,10 @@ local function OptionsScreen_AddModsTab(self)
                 table.sort(list_client, ModSortingComparators[SELECTED_SORTING_METHOD])
             end
             for _,modname in ipairs(list_client) do
-                _AddModEntry(modname)
+                AddModEntry(modname)
             end
+
+            -- SERVER MODS
             self.pages.mods.mod_entries:AddChild(OptionsScreenCategoryTitle(self.rowWidth, "Server Mods"))
             local list_server = GLOBAL.KnownModIndex:GetServerModNames()
             if SELECTED_SORTING_METHOD == 7 then
@@ -159,13 +153,12 @@ local function OptionsScreen_AddModsTab(self)
 
         -- refresh the nav bar again
 	    local icon_size = GLOBAL.FONTSIZE.OPTIONS_SCREEN_TAB * 1.1
-
         self.nav_tabs
 		    :SetTabSize(nil, icon_size)
             :Layout() -- For the cycle icons
             :LayoutBounds("center", "top", self.panel_bg)
             :Offset(0, -90)
-        
+
         self.pages.mods = self.scrollContents:AddChild(Widget("Page Mods"))
         self.tabs.mods.page = self.pages.mods
 
@@ -197,11 +190,11 @@ local function OptionsScreen_AddModsTab(self)
             :_SetValue(SELECTED_SORTING_METHOD)
             :SetOnValueChangeFn(function(data)
                 SELECTED_SORTING_METHOD = data
-                _LayoutModEntries()
+                LayoutModEntries()
             end)
 
         self.pages.mods.mod_entries = self.pages.mods:AddChild(Widget("Mod Entries"))
-        _LayoutModEntries()
+        LayoutModEntries()
     end
 end
 
--- modmenu/scripts/modsortingcomparators.lua
+++ modmenu/scripts/modsortingcomparators.lua
@@ -1,12 +1,12 @@
--- TODO: can this be better organized?
-
 local ModSortingComparators = {
     function(a, b) -- 1: alphabetical
         return KnownModIndex:GetModFancyName(a) < KnownModIndex:GetModFancyName(b)
     end,
+    
     function(a, b) -- 1: reversed alphabetical
         return KnownModIndex:GetModFancyName(a) > KnownModIndex:GetModFancyName(b)
     end,
+
     function(a, b) -- 3: author
         local author_a = "unknown"
         if KnownModIndex and KnownModIndex.GetModInfo and KnownModIndex:GetModInfo(a) and KnownModIndex:GetModInfo(a)["author"] then
@@ -18,6 +18,7 @@ local ModSortingComparators = {
         end
         return author_a < author_b
     end,
+
     function(a, b) -- 4: reversed author
         local author_a = "unknown"
         if KnownModIndex and KnownModIndex.GetModInfo and KnownModIndex:GetModInfo(a) and KnownModIndex:GetModInfo(a)["author"] then
@@ -29,6 +30,7 @@ local ModSortingComparators = {
         end
         return author_a > author_b
     end,
+
     function(a, b) -- 5: enabled first
         local enabled_a = KnownModIndex.savedata.known_mods[a].enabled
         local enabled_b = KnownModIndex.savedata.known_mods[b].enabled
@@ -37,6 +39,7 @@ local ModSortingComparators = {
         end
         return false
     end,
+
     function(a, b) -- 6: disabled first
         local enabled_a = KnownModIndex.savedata.known_mods[a].enabled
         local enabled_b = KnownModIndex.savedata.known_mods[b].enabled
@@ -45,7 +48,8 @@ local ModSortingComparators = {
         end
         return false
     end,
-    function(a, b) -- 7: favorited first
+
+    function(a, b) -- 7: favorites first
         local favorited_a = Profile:IsModFavorited(a)
         local favorited_b = Profile:IsModFavorited(b)
         if favorited_a and not favorited_b then
