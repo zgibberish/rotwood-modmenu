3d44cf57db1179d393352696fe9b14552a69df6e
gibberish <gbjgms@gmail.com>
2024-10-15 23:11:46 +0700

    Changed version number to 2.0 Major version number change due to transition to the new modloader

--- modmenu/modinfo.lua
+++ modmenu/modinfo.lua
@@ -1,7 +1,7 @@
 name = "Mod Menu"
 description = "GUI to view and manage installed mods, adds a \"Mods\" tab to the options screen."
 author = "gibberish"
-version = "1.4"
+version = "2.0"
 api_version = 10
 
 dst_compatible = false
